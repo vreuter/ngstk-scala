fe3939fa1a537e9ea671a5cc2c7015bf249b588e
Vince Reuter <vince.reuter@gmail.com>
2020-12-18 02:39:45 -0500

    better doc

--- src/main/scala/Intersection.scala
+++ src/main/scala/Intersection.scala
@@ -16,6 +16,11 @@ object Intersection {
   /**
    * Drag arbitrary, coordinate-paired data "through" a read, retaining only those elements with coordinate "in bounds."
    *
+   * Note here the coordinate scheme. Per the {@code htskdk} docs, the {@code getStart} and {@code getEnd} methods on 
+   * {@code SAMRecord} return 1-based (inclusive) positions, while the coordinate in each {@code sites} element will likely 
+   * come from a format like BED, where the cordinates are 0-based left-inclusive. Also BEWARE of the fact that to return 
+   * just the basic {@code Int} type, the star/end methods on {@code SAMRecord} return 0 if there's no alignment.
+   *
    * @tparam A The arbirary data type for the piece of data paired with each coordinate / genome position
    * @tparam CC The collection type constructor ("Collection Constructor")
    * @param sites The collection of pairs genome coordinate and arbitrary datum
