4c828f72b0ce973ec396fbeea60b9acf081ee53e
Vince Reuter <vince.reuter@gmail.com>
2021-03-10 22:14:54 -0500

    resolve 2.12/2.13 collections API discrepancy

--- src/main/scala/ReadGroupData.scala
+++ src/main/scala/ReadGroupData.scala
@@ -33,21 +33,28 @@ object ReadGroupData extends LazyLogging {
     libReads: List[MetadataColumnParser])(metadata: File, sep: String)(
     fastqFolder: File, fqFile2Sample: File => Either[String, SampleName]): List[(SampleName, List[ReadGroupData])] = {
     import cats.{ Alternative, Order }, cats.instances.either._, cats.instances.int._, cats.instances.list._, cats.instances.string._
+    
     val boundMeta: Either[String, Map[SampleName, (PlatformName, LibId)]] = for {
       samples <- MetadataColumnParser.tryParseSampleNames(sampleReads)(metadata, sep)
       platforms <- MetadataColumnParser.tryParsePlatformNames(platReads)(metadata, sep)
       libraries <- MetadataColumnParser.tryParseLibraryIds(libReads)(metadata, sep)
     } yield bindMetadataToSampleNames(samples, platforms, libraries).toMap
+    
     val (bads, goods): (List[String], List[(SampleName, Flowcell, Lane)]) = Alternative[List].separate(
       fastqFolder.listFiles.toList map { fq => for {
         sample <- fqFile2Sample(fq)
         flow <- tryFlowcellFromFastq(fq)
         lane <- fastq2LaneNumber(fq)
       } yield (sample, flow, lane) } )
+    
     if (bads.nonEmpty) throw new Exception(s"${bads.size} error(s): ${bads mkString ", "}")
+    
     implicit val ordLane: Ordering[Lane] = Order.by((_: Lane).get).toOrdering
-    val flowAndLaneBySample: Map[SampleName, List[(Flowcell, Lane)]] = 
+    val flowAndLaneBySample: Map[SampleName, List[(Flowcell, Lane)]] = {
+      import scala.collection.compat._    // for .view.mapValues from 2.13, for 2.12 compat
       goods.groupBy(_._1).view.mapValues(_.map(tup => tup._2 -> tup._3).sortBy(_._2)).toMap
+    }
+    
     implicit val ordSampleName: Ordering[SampleName] = Order.by((_: SampleName).get).toOrdering
     boundMeta.fold( err => throw new Exception(err), bound => {
       flowAndLaneBySample.toList.sortBy(_._1).foldRight(List.empty[(SampleName, List[ReadGroupData])]){
